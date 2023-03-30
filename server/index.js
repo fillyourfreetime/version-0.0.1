798b9ce2ea246fde5719d7b0c1b6406f7845ae73
tijmi <77972031+tijmi@users.noreply.github.com>
2023-03-30 10:46:51 +0200

    index.js klaar voor users

--- server/index.js
+++ server/index.js
@@ -4,3 +4,14 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
+const db = require("./models");
+
+const Userrouter = require("./models/users");
+app.use("/users", Userrouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("server up on port 3001")
+    })
+})
