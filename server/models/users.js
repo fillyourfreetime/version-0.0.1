166ebd14a1e55076cc78786097247f77bb20176a
tijmi <77972031+tijmi@users.noreply.github.com>
2023-03-30 12:01:52 +0200

    users tabel klaar voor nu

--- server/models/users.js
+++ server/models/users.js
@@ -12,6 +12,27 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    age: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    phonenumber: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+    },
+    emailverification: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0,
+    },
   });
+  //Users.associate = (models) => {
+  //  Users.hasMany(models.Likes, {
+  //    onDelete: "cascade",
+  //  });
+  //  Users.hasMany(models.followers, {
+  //    onDelete:"cascade"
+  //  })
+  //};
   return Users;
 };
